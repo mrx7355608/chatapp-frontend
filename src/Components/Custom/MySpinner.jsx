d4c90e3343764c24e8cb2de5cf5d77e37ff61738
mrx7355608 <mrx7355608@gmail.com>
2022-11-13 12:21:33 +0500

    changed empty color of spinner

--- src/Components/Custom/MySpinner.jsx
+++ src/Components/Custom/MySpinner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function MySpinner() {
     return (
         <Flex alignItems="center" justify="center" w="100vw" h="100vh">
-            <Spinner color="#F3664C" />
+            <Spinner size="lg" color="#F3664C" emptyColor="orange.200" />
         </Flex>
     );
 }
