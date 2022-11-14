6c5ca5edd39814a33a7076579e460f58031ff7c3
mrx7355608 <mrx7355608@gmail.com>
2022-11-14 22:55:45 +0500

    created action for signup and added validations

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -11,100 +11,163 @@ import {
     Text,
     Spinner,
 } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Form, Link, useActionData, useNavigation } from "react-router-dom";
 
 export default function Signup() {
+    const navigation = useNavigation();
     const { colorMode } = useColorMode();
-    const [loading, setLoading] = React.useState(false);
+
+    const signupErrors = useActionData();
 
     return (
         <Flex alignItems="center" justify="center" w="100vw" minHeight="100vh">
             <Container
-                maxWidth="450px"
+                maxWidth="400px"
                 rounded="lg"
                 my="4"
-                p="6"
+                p="4"
                 bgColor={colorMode === "light" ? "gray.50" : "gray.700"}
             >
                 <Heading textAlign="center" mt="6" mb="8">
                     Signup
                 </Heading>
-                <Input
-                    type="text"
-                    variant="filled"
-                    bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
-                    pt="1"
-                    size="lg"
-                    mb="3"
-                    placeholder="First name"
-                />
-                <Input
-                    type="text"
-                    variant="filled"
-                    bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
-                    pt="1"
-                    size="lg"
-                    mb="3"
-                    placeholder="Last name"
-                />
-                <Input
-                    type="text"
-                    variant="filled"
-                    bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
-                    pt="1"
-                    size="lg"
-                    mb="3"
-                    placeholder="Username"
-                />
-                <Input
-                    type="password"
-                    variant="filled"
-                    bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
-                    pt="1"
-                    size="lg"
-                    mb="3"
-                    placeholder="Password"
-                />
-                <Input
-                    type="password"
-                    bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
-                    variant="filled"
-                    pt="1"
-                    size="lg"
-                    mb="8"
-                    placeholder="Confirm Password"
-                />
-                {loading ? (
-                    <Button
-                        disabled
+
+                <Form method="post">
+                    {signupErrors?.apiError && (
+                        <Text
+                            w="full"
+                            p="3"
+                            rounded="md"
+                            bgColor="red.200"
+                            color="red.800"
+                            pt="3.5"
+                            mt="3"
+                        >
+                            {signupErrors?.apiError}
+                        </Text>
+                    )}
+                    <Input
+                        type="text"
+                        name="fname"
+                        variant="filled"
+                        bgColor={
+                            colorMode === "light" ? "gray.200" : "gray.600"
+                        }
+                        pt="1.5"
                         size="md"
-                        w="full"
-                        color="white"
-                        bgColor="#F3664C"
-                    >
-                        <Spinner mr="2" size="sm" />
-                        <Text pt="1">Signing up...</Text>
-                    </Button>
-                ) : (
-                    <Button
+                        mt="3"
+                        placeholder="First name"
+                        border={signupErrors?.fname && "2px"}
+                    />
+                    {signupErrors?.fname && (
+                        <Text mt="1" fontSize="sm" as="b" color="red.500">
+                            {signupErrors.fname}
+                        </Text>
+                    )}
+                    <Input
+                        type="text"
+                        name="lname"
+                        variant="filled"
+                        bgColor={
+                            colorMode === "light" ? "gray.200" : "gray.600"
+                        }
+                        pt="1.5"
                         size="md"
-                        pt="1"
-                        w="full"
-                        color="white"
-                        bgColor="#F3664C"
-                    >
-                        Signup
-                    </Button>
-                )}
+                        mt="3"
+                        placeholder="Last name"
+                    />
+                    {signupErrors?.lname && (
+                        <Text mt="1" fontSize="sm" as="b" color="red.500">
+                            {signupErrors.lname}
+                        </Text>
+                    )}
+                    <Input
+                        type="text"
+                        name="username"
+                        variant="filled"
+                        bgColor={
+                            colorMode === "light" ? "gray.200" : "gray.600"
+                        }
+                        pt="1.5"
+                        size="md"
+                        mt="3"
+                        placeholder="Username"
+                    />
+                    {signupErrors?.username && (
+                        <Text mt="1" fontSize="sm" as="b" color="red.500">
+                            {signupErrors.username}
+                        </Text>
+                    )}
+                    <Input
+                        type="password"
+                        name="password"
+                        variant="filled"
+                        bgColor={
+                            colorMode === "light" ? "gray.200" : "gray.600"
+                        }
+                        pt="1.5"
+                        size="md"
+                        placeholder="Password"
+                        mt="3"
+                    />
+                    {signupErrors?.password && (
+                        <Text mt="1" fontSize="sm" as="b" color="red.500">
+                            {signupErrors.password}
+                        </Text>
+                    )}
+                    <Input
+                        type="password"
+                        name="confirm_password"
+                        bgColor={
+                            colorMode === "light" ? "gray.200" : "gray.600"
+                        }
+                        variant="filled"
+                        pt="1.5"
+                        size="md"
+                        mt="3"
+                        placeholder="Confirm Password"
+                    />
+                    {signupErrors?.confirmPassword && (
+                        <Text mt="1" fontSize="sm" as="b" color="red.500">
+                            {signupErrors.confirmPassword}
+                        </Text>
+                    )}
+                    {navigation.state === "loading" ? (
+                        <Button
+                            mt="8"
+                            disabled
+                            size="md"
+                            w="full"
+                            color="white"
+                            type="button"
+                            bgColor="#F3664C"
+                        >
+                            <Spinner mr="2" size="sm" />
+                            <Text pt="1">Signing up...</Text>
+                        </Button>
+                    ) : (
+                        <Button
+                            mt="8"
+                            type="submit"
+                            size="md"
+                            pt="1"
+                            w="full"
+                            color="white"
+                            bgColor="#F3664C"
+                        >
+                            Signup
+                        </Button>
+                    )}
+                </Form>
 
                 <Text
                     textAlign="center"
                     color={colorMode === "light" ? "gray.700" : "gray.200"}
                     my="5"
                 >
-                    Already have an account?{" "}
+                    Already have an account?
                     <Text as="b" color="red.500">
-                        <Link to="/login">Login</Link>
+                        <Link to="/login"> Login</Link>
                     </Text>
                 </Text>
             </Container>
---
+++ src/actions/signup.js
@@ -0,0 +1,63 @@
+import { redirect } from "react-router-dom";
+
+const signupAction = async ({ request }) => {
+    const formData = await request.formData();
+    const fname = formData.get("fname");
+    const lname = formData.get("lname");
+    const username = formData.get("username");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm_password");
+
+    const errors = {};
+
+    // first name
+    if (typeof fname !== "string" || fname.length < 4) {
+        errors.fname = "First name should be at least 4 characters long";
+    } else if (typeof lname !== "string" || lname.length < 4) {
+        errors.lname = "Last name should be at least 4 characters long";
+    } else if (typeof username !== "string" || username.length < 5) {
+        errors.username = "Username is too short, use 5 characters at least";
+    } else if (typeof password !== "string" || password.length < 8) {
+        errors.password = "Password should be at least 8 characters long";
+    } else if (confirmPassword !== password) {
+        errors.confirmPassword = "Passwords do not match";
+    }
+
+    if (Object.keys(errors).length) return errors;
+
+    // Signup api call
+    try {
+        const userData = {
+            fname,
+            lname,
+            username,
+            password,
+            confirm_password: confirmPassword,
+        };
+        const url = `${import.meta.env.VITE_API_URL}/auth/signup`;
+        const options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        };
+        const apiResponse = await fetch(url, options);
+        if (!apiResponse.ok) {
+            const apiData = await apiResponse.json();
+            errors.apiError = apiData.message;
+            return errors;
+        }
+        return redirect("/login");
+    } catch (err) {
+        if (err.name === "TypeError") {
+            throw new Response("It seems that the server is down!", {
+                status: 500,
+            });
+        }
+        throw new Response("Something went wrong!", { status: 500 });
+    }
+};
+
+export default signupAction;
