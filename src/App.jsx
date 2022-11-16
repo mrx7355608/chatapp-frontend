d37a36a427b2f10f5697dfdf1537274a3456f283
mrx7355608 <mrx7355608@gmail.com>
2022-11-16 05:28:35 +0500

    presisting user on page reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
-
+import { setAccessToken } from "./accessToken";
+// Contexts
+import UserContext from "./Contexts/UserContext";
 // Components
 import Navbar from "./Components/Navbar";
+// Requests functions
+import refreshToken from "./requests/refreshToken";
+import fetchUser from "./requests/fetchUser";
+import MySpinner from "./Components/Custom/MySpinner";
 
 function App() {
+    const [user, setUser] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        // Refresh token
+        // eslint-disable-next-line wrap-iife, consistent-return
+        (async function setup() {
+            const data = await refreshToken();
+            if (!data) return setLoading(false);
+            setAccessToken(data.accessToken);
+            // TODO: fetch user
+            const response = await fetchUser();
+            setUser(response.data);
+            setLoading(false);
+        })();
+    }, []);
+
+    if (loading) return <MySpinner />;
     return (
         <Box>
-            <Navbar />
-            <Outlet />
+            <UserContext.Provider value={{ user, setUser }}>
+                <Navbar />
+                <React.Suspense fallback={<MySpinner />}>
+                    <Outlet />
+                </React.Suspense>
+            </UserContext.Provider>
         </Box>
     );
 }
