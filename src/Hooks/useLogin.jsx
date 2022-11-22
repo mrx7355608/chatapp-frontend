5320be6cfb76392ae5abc4ac2fb8bd6c28e57eec
mrx7355608 <mrx7355608@gmail.com>
2022-11-22 13:25:11 +0500

    refactored

--- src/Hooks/useLogin.jsx
+++ src/Hooks/useLogin.jsx
@@ -1,13 +1,18 @@
 import axios from "axios";
-import { redirect } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Contexts/AuthContext";
 
 export default function useLogin() {
     const { dispatch } = useAuth();
+    const navigateTo = useNavigate();
+
     // eslint-disable-next-line consistent-return
     const login = (loginData) => {
+        // Shows loading state
         dispatch({ type: "MAKE_REQUEST" });
+
         const { username, password } = loginData;
+        // Invalid data error handling
         if (!username) {
             return dispatch({
                 type: "LOGIN",
@@ -23,34 +28,41 @@ export default function useLogin() {
             });
         }
 
+        // Making request
         axios
             .post("http://localhost:4000/auth/login", loginData, {
                 withCredentials: true,
             })
             .then((resp) => {
+                // If request is successfull then update token
                 dispatch({
                     type: "LOGIN",
-                    accessToken: resp.data.accessToken,
+                    token: resp.data.accessToken,
                     error: {},
                 });
-                return redirect("/"); // WARNING: Didn't redirect
+
+                return navigateTo("/");
             })
             .catch((err) => {
+                const dispatchAction = {
+                    type: "LOGIN",
+                    accessToken: undefined,
+                };
+
+                // If api returns an error
                 if (err.response) {
                     const { message } = err.response.data;
                     return dispatch({
-                        type: "LOGIN",
-                        accessToken: undefined,
+                        ...dispatchAction,
                         error: { type: "api", message },
                     });
                 }
+
+                // If there is some server related error
+                const message = "It seems that the server is down";
                 return dispatch({
-                    type: "LOGIN",
-                    accessToken: undefined,
-                    error: {
-                        type: "api",
-                        message: "It seems that the server is down",
-                    },
+                    ...dispatchAction,
+                    error: { type: "api", message },
                 });
             });
     };
